f10bfe1965acb713bdc5ea69b1949ec8c94345fc
国华 王 <wgh0918@foxmail.com>
2017-10-06 22:43:32 +0800

    1

--- department_student/departmentstudent.cpp
+++ department_student/departmentstudent.cpp
@@ -9,9 +9,35 @@
 #include <time.h>
 #include <map>
 
+using namespace std;
+
+struct Student {
+	string Sno;
+	string FreeTime[50];
+	string ApplicationsDepartment[5];
+	string Tags[10];
+	string AlreadyDepartment[5];
+	int NumDepartment;
+};
+
+struct Department {
+	string Did;
+	string EventSchedules[50];
+	string Tags[10];
+	int LitmitNum;
+	int AMember[300];
+	int BMember[300];
+	int SNum;
+};
+
+void ReadIN()
+{
+
+}
 
 int main()
 {
+
     return 0;
 }
 
